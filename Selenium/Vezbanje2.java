55eaa1348905bd0315e614a96897fc4b54aea8e2
VesnaBogdanovic <61417590+VesnaBogdanovic@users.noreply.github.com>
2021-01-27 20:18:58 +0100

    Update Vezbanje2.java

--- Selenium/Vezbanje2.java
+++ Selenium/Vezbanje2.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Vezbanje2 {
 
 	public static void main(String[] args) {
-	// Idem na youtube, 5 puta klikcem na dugme refresh i tek onda zatvaram pretrazivac
+	// Idem na youtube, 5 puta klikcem na dugme refresh i tek onda zatvaram pretrazivac.
 		
 		System.setProperty("webdriver.chrome.driver", "src\\vezbanjeSelenium\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
